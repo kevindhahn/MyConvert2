87710d3d3dde899bb47548236da327693838847e
kevindhahn <66223213+kevindhahn@users.noreply.github.com>
2020-06-06 14:59:13 -0500

    Add files via upload
    Visual Studio 15 project with function to convert from integer to string and string to integer.  It also includes a function that uses exceptions.

---
+++ MyConvert/MyConvert.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConvert
+{
+    public class MyConvert
+    {
+        public static string IntegerToString(int value)
+        {
+            string result = System.String.Empty;
+            bool isNegative = false;
+
+            if (value < 0)
+            {
+                isNegative = true;
+                value = Math.Abs(value);
+            }
+
+            // integers can only be base 10
+            int lsv = value % 10;
+            do
+            {
+                // 48 is the ascii code for zero
+                int ascii = 48 + lsv;
+                char c = (char)ascii;
+                result = c.ToString() + result;
+
+                value = value / 10;
+                lsv = value % 10;
+            }
+            while (lsv > 0);
+
+            if (isNegative == true)
+            {
+                result = "-" + result;
+            }
+
+            return result;
+        }
+
+        public static int StringToIntegerWithExceptions(string value)
+        {
+            bool isNegative = false;
+            int result = 0;
+            int index = 0;
+
+            // Make sure a character exists
+            if (value.Length == 0)
+            {
+                throw new ArgumentOutOfRangeException("value", "Parameter is an empty string");
+            }
+
+            // If first character is not valid exit
+            if (false == MyConvert.IsValid10(value[0]))
+            {
+                if (value[0] != 45)
+                {
+                    throw new ArgumentOutOfRangeException("value", "Parameter starts with invalid character");
+                }
+                else
+                {
+                    // If dash then negative number
+                    isNegative = true;
+                    index++;
+                }
+            }
+
+            // If valid character loop through characters.
+            for (int i = index; i < value.Length; i++)
+            {
+                char c = value[i];
+                if (false == MyConvert.IsValid10(c))
+                {
+                    throw new ArgumentOutOfRangeException("value", "Parameter contains an invalid character");
+                }
+
+                result = (result * 10) + (c - 48);
+            }
+
+            // If this was negative add to results
+            if (true == isNegative)
+            {
+                result = result * -1;
+            }
+
+            return result;
+        }
+
+
+        public static int StringToInteger(string value)
+        {
+            bool isNegative = false;
+            int result = 0;
+            int index = 0;
+
+            // Make sure a character exists
+            if (value.Length == 0)
+            {
+                return result;
+            }
+
+            // If first character is not valid exit
+            if (false == MyConvert.IsValid10(value[0]))
+            {
+                if (value[0] != 45) 
+                {
+                    return result;
+                }
+                else
+                {
+                    // If dash then negative number
+                    isNegative = true;
+                    index++;
+                }
+            }
+
+            // If valid character loop through characters.
+            for (int i = index; i < value.Length; i++)
+            {
+                char c = value[i];
+                if (false == MyConvert.IsValid10(c))
+                { 
+                    break;
+                }
+
+                result = (result * 10) + (c - 48);
+            }
+
+            // If this was negative add to results
+            if (true == isNegative)
+            {
+                result = result * -1;
+            }
+
+            return result;
+        }
+
+        public static bool IsValid10(char c)
+        {
+            return ((48 <= c) && (c <= 57));
+        }
+
+    }
+}
---
+++ UnitTestMyConvert/UTMyConvert.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestMyConvert
+{
+    [TestClass]
+    public class UTMyConvert
+    {
+        /*
+         * ------------------------------------------------------------------------------
+         * Integer to String Test Cases
+         * ------------------------------------------------------------------------------
+         */
+        [TestMethod]
+        public void IntegerToStringPositive()
+        {
+            string expected = "123";
+            int value = 123;
+
+            string result = MyConvert.MyConvert.IntegerToString(value);
+            Assert.AreEqual(expected, result, "Positive Integer To String Failed");
+        }
+
+        [TestMethod]
+        public void IntegerToStringZero()
+        {
+            string expected = "0";
+            int value = 0;
+
+            string result = MyConvert.MyConvert.IntegerToString(value);
+            Assert.AreEqual(expected, result, "Zero Integer To String Failed");
+        }
+
+        [TestMethod]
+        public void IntegerToStringNegative()
+        {
+            string expected = "-123";
+            int value = -123;
+
+            string result = MyConvert.MyConvert.IntegerToString(value);
+            Assert.AreEqual(expected, result, "Negative Integer To String Failed");
+        }
+
+        /*
+         * ------------------------------------------------------------------------------
+         * String to Integer with no Exceptions Test Cases
+         * ------------------------------------------------------------------------------
+         */
+        [TestMethod]
+        public void StringToIntegerInvalidStart()
+        {
+            string value = "x12";
+            int expected = 0;
+
+            int result = MyConvert.MyConvert.StringToInteger(value);
+            Assert.AreEqual(expected, result, "Invalid String To Integer Invalid Start Failed");
+        }
+
+        [TestMethod]
+        public void StringToIntegerInvalidMiddle()
+        {
+            string value = "1xy2";
+            int expected = 1;
+
+            int result = MyConvert.MyConvert.StringToInteger(value);
+            Assert.AreEqual(expected, result, "Invalid String To Integer Invalid Middle Failed");
+        }
+
+        [TestMethod]
+        public void StringToIntegerInvalidEnd()
+        {
+            string value = "145xy";
+            int expected = 145;
+
+            int result = MyConvert.MyConvert.StringToInteger(value);
+            Assert.AreEqual(expected, result, "Invalid String To Integer Invalid End Failed");
+        }
+
+        [TestMethod]
+        public void StringToIntegerNegative()
+        {
+            string value = "-145";
+            int expected = -145;
+
+            int result = MyConvert.MyConvert.StringToInteger(value);
+            Assert.AreEqual(expected, result, "Invalid String To Integer Negative Failed");
+        }
+
+        [TestMethod]
+        public void StringToIntegerZero()
+        {
+            string value = "0";
+            int expected = 0;
+
+            int result = MyConvert.MyConvert.StringToInteger(value);
+            Assert.AreEqual(expected, result, "Invalid String To Integer Zero Failed");
+        }
+
+        [TestMethod]
+        public void StringToIntegerPositive()
+        {
+            string value = "145";
+            int expected = 145;
+
+            int result = MyConvert.MyConvert.StringToInteger(value);
+            Assert.AreEqual(expected, result, "Invalid String To Integer Positive Failed");
+        }
+
+        [TestMethod]
+        public void StringToIntegerEmpty()
+        {
+            string value = "";
+            int expected = 0;
+
+            int result = MyConvert.MyConvert.StringToInteger(value);
+            Assert.AreEqual(expected, result, "Invalid String To Integer Positive Failed");
+        }
+
+        /*
+         * ------------------------------------------------------------------------------
+         * Integer to String Test Cases
+         * ------------------------------------------------------------------------------
+         */
+        [TestMethod]
+        public void IsValidLessThan()
+        {
+            char value = '(';
+            bool expected = false;
+
+            bool result = MyConvert.MyConvert.IsValid10(value);
+            Assert.AreEqual(expected, result, "IsValid Less Than Failed");
+        }
+
+        [TestMethod]
+        public void IsValidGreaterThan()
+        {
+            char value = 'W';
+            bool expected = false;
+
+            bool result = MyConvert.MyConvert.IsValid10(value);
+            Assert.AreEqual(expected, result, "IsValid Less Than Failed");
+        }
+
+        [TestMethod]
+        public void IsValidTrue()
+        {
+            char value = '5';
+            bool expected = true;
+
+            bool result = MyConvert.MyConvert.IsValid10(value);
+            Assert.AreEqual(expected, result, "IsValid Less Than Failed");
+        }
+
+        /*
+         * ------------------------------------------------------------------------------
+         * String to Integer with Exceptions test cases 
+         * ------------------------------------------------------------------------------
+         */
+        [TestMethod]
+        public void StringToIntegerWExpInvalidStart()
+        {
+            string value = "x12";
+            try
+            {
+                int result = MyConvert.MyConvert.StringToIntegerWithExceptions(value);
+                Assert.Fail("StringToIntegerWithExceptions did not throw exception");
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual(typeof(ArgumentOutOfRangeException), ex.GetType(), "Incorrect exception thrown");
+
+                ArgumentOutOfRangeException oor = (ArgumentOutOfRangeException)ex;
+                Assert.AreEqual("value", oor.ParamName, "Incorrect exception value");
+            }
+        }
+
+        [TestMethod]
+        public void StringToIntegerWExpInvalidMiddle()
+        {
+            string value = "1xy2";
+
+            try
+            {
+                int result = MyConvert.MyConvert.StringToIntegerWithExceptions(value);
+                Assert.Fail("StringToIntegerWithExceptions did not throw exception");
+
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual(typeof(ArgumentOutOfRangeException), ex.GetType(), "Incorrect exception thrown");
+
+                ArgumentOutOfRangeException oor = (ArgumentOutOfRangeException)ex;
+                Assert.AreEqual("value", oor.ParamName, "Incorrect exception value");
+            }
+        }
+
+        [TestMethod]
+        public void StringToIntegerWExpInvalidEnd()
+        {
+            string value = "145xy";
+
+            try
+            {
+                int result = MyConvert.MyConvert.StringToIntegerWithExceptions(value);
+                Assert.Fail("StringToIntegerWithExceptions did not throw exception");
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual(typeof(ArgumentOutOfRangeException), ex.GetType(), "Incorrect exception thrown");
+
+                ArgumentOutOfRangeException oor = (ArgumentOutOfRangeException)ex;
+                Assert.AreEqual("value", oor.ParamName, "Incorrect exception value");
+            }
+        }
+
+        [TestMethod]
+        public void StringToIntegerWExpNegative()
+        {
+            string value = "-145";
+            int expected = -145;
+
+            try
+            {
+                int result = MyConvert.MyConvert.StringToIntegerWithExceptions(value);
+                Assert.AreEqual(expected, result, "Invalid String To Integer Negative Failed");
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Unexpected exception {0}", ex);
+            }
+        }
+
+        [TestMethod]
+        public void StringToIntegerWExpZero()
+        {
+            string value = "0";
+            int expected = 0;
+
+            try
+            {
+                int result = MyConvert.MyConvert.StringToIntegerWithExceptions(value);
+                Assert.AreEqual(expected, result, "Invalid String To Integer Zero Failed");
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Unexpected exception {0}", ex);
+            }
+        }
+
+        [TestMethod]
+        public void StringToIntegerWExpPositive()
+        {
+            string value = "145";
+            int expected = 145;
+
+            try
+            {
+                int result = MyConvert.MyConvert.StringToIntegerWithExceptions(value);
+                Assert.AreEqual(expected, result, "Invalid String To Integer Positive Failed");
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Unexpected exception {0}", ex);
+            }
+
+        }
+
+        [TestMethod]
+        public void StringToIntegerWExpEmpty()
+        {
+            string value = "";
+            int expected = 0;
+
+            try
+            {
+                int result = MyConvert.MyConvert.StringToIntegerWithExceptions(value);
+                Assert.AreEqual(expected, result, "Invalid String To Integer Positive Failed");
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual(typeof(ArgumentOutOfRangeException), ex.GetType(), "Incorrect exception thrown");
+
+                ArgumentOutOfRangeException oor = (ArgumentOutOfRangeException)ex;
+                Assert.AreEqual("value", oor.ParamName, "Incorrect exception value");
+            }
+        }
+    }
+}
